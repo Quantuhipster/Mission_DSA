71b22a56685ae212bef650ff53c0e367eab0ed67
ujjwal <ujjwaldwivedi129@gmail.com>
2026-08-03 22:57:57 +0530

    Time: 10 ms (74.03%), Space: 12.5 MB (19.38%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def convert(self, s, numRows):
+
+        if numRows == 1 or len(s) <= numRows:
+            return s
+        result = [''] * numRows
+        index = 0
+        step = 1
+
+        for char in s:
+            result [index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                    step = -1
+            index += step
+
+        return ''.join(result)
